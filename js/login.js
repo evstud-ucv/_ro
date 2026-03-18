9ef0c577cb76d9026ef9deab8774776cbeacb65a
evstud-ucv <rebecadinu1307@gmail.com>
2026-03-18 23:58:25 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,1 @@
-var pas = "cnp";
-
-$(document).ready(function() {
-    $("#frmVerCnp").off("submit").on("submit", function(e) {
-        e.preventDefault();
-        
-        if (pas === "cnp") {
-            $(".only-cnp").hide();
-            $(".only-pass").removeClass("collapse").show();
-            $("#inputPass").prop("required", false);
-            pas = "pass";
-        } else {
-            window.location.href = "dashboard.html";
-        }
-    });
-});
+// intentionat gol
